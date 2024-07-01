9fc2cb7838c93879ab816135d40fd98c21fece75
Vegot795 <167176338+Vegot795@users.noreply.github.com>
2024-07-01 21:09:07 +0200

    p2

--- skrypt.sh
+++ skrypt.sh
@@ -4,3 +4,11 @@ show_date() {
         date'+%Y-%m-%d'
 }
 
+create_logs() {
+	local script_name=$(basename "$0-")
+	local timestamp=$(date '+%Y-%m-%d %T' )
+
+	for ((i=1, i<= 10; i++)); do
+		echo "log $iL $script_name, $timestamp" > "log_$i.txt"
+	done
+}
